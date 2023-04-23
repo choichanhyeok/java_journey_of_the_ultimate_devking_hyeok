471992b5b33a297d7daef7541dbc360e1a8dbb4e
hyeoKing <cksgurwkd12@naver.com>
2023-04-24 00:21:28 +0900

    feat: add learn area about test for oop

--- java_basic/java_string/Main.java
+++ java_basic/java_string/Main.java
@@ -2,8 +2,8 @@ import study_log.WhatDifferencesCharArrayAndString;
 import study_log.UsingAlphabetCounter;
 import study_log.AlphabetCount;
 
-public class Main{
-    public static void main(String[] hyeok){
+public class Main {
+    public static void main(String[] hyeok) {
         // WhatDifferencesCharArrayAndString.question2744();
         // UsingAlphabetCounter.isAnagram();
         AlphabetCount.solve1157();
