8096d1529869e12af258e624da13fdbf07f39de3
hyeok <cksgurwkd12@naver.com>
2023-04-26 23:50:02 +0900

    feat: api practice

---
+++ java_basic/a.js
@@ -0,0 +1,41 @@
+
+let userInfo = `{
+    "userInfoList" : [
+        {
+            "name" : "hyeok",
+            "song" : "jibre"
+        },
+        {
+            "name" : "gain",
+            "song" : "trot123"
+        }
+    ]
+}
+`
+
+console.log(JSON.parse(userInfo));
+
+
+
+
+
+
+
+
+
+// let a = `{
+//         "지브리 OST 리스트": [
+//             {
+//                 "name" : "마녀 배달부 키키",
+//                 "song" : "따스함이 어쩌구"
+//             },
+//             {
+//                 "name" : "하울의 움직이는 성",
+//                 "song" : "움직이는 회전목마"
+
+//             }
+//         ]
+//     }`
+
+// a = JSON.parse(a);
+// console.log(a);
