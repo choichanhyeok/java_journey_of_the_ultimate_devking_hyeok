8e1e303eba5bea088d5baef6e86be6eefdb17279
hyeok <cksgurwkd12@naver.com>
2023-05-07 21:00:44 +0900

    feat: 어쩌다보니 들어오게 된 노드 js 코드 ..

---
+++ java_basic/add_nodeJs/basic_nodJs/main.js
@@ -0,0 +1,9 @@
+
+// let m = require('./math-tools.js');
+// console.log(m.add(1, 2));
+
+
+const cowsay = require("cowsay");
+console.log(cowsay.say({
+    text: "개발자중에 왕은 최찬혁이요우 ~~"
+}))
---
+++ java_basic/add_nodeJs/basic_nodJs/math-tools.js
@@ -0,0 +1,8 @@
+
+
+const add = function(a, b){
+    return a + b;
+}
+
+exports.add = add;
+
